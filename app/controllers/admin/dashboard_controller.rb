6355e1767446843aa0a4a3a51a5f4dc5728e9473
Alex <your@email.com>
2022-04-09 21:11:36 +0000

    added admin dashboard data display

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTHENTICATION_USERNAME'], password: ENV['AUTHENTICATION_PASSWORD'], except: [:new, :show, :edit, :create]
   def show
+    @num_products = Product.count()
+    @num_categories = Category.count()
   end
 end
