e1b8e13ef926f4b8261cf4f54d0d58041d903dee
Alex <your@email.com>
2022-04-13 14:33:57 +0000

    added user testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe User, type: :model do
   
   describe "Validations" do
     it "saves succesfully if all fields are valid" do
-      user_params = { first_name: "Pineapple", last_name: "Pizza", email: "pineapple@pizza.com", password: "123456789", password_confirmation: "123456789" }
+      user_params = { first_name: "Jack", last_name: "Sparrow", email: "jack@sparrow.com", password: "123456789", password_confirmation: "123456789" }
       @user = User.new(user_params)
 
       expect(@user.save).to be true
     end
 
     it "should not create new user when password and password_confirmation don't match" do
-      user_params = { first_name: "Captain", last_name: "Picard", email: "uss_enterprise@starfleet.com", password: "123456789", password_confirmation: "987654321" }
+      user_params = { first_name: "William", last_name: "Turner", email: "willturner@blackpearl.com", password: "123456789", password_confirmation: "987654321" }
       @user = User.new(user_params)
 
       expect(@user.save).to be false
     end
 
     it "should not create user when password length is too short" do
-      user_params = { first_name: "William", last_name: "Riker", email: "wt.riker@starfleet.com", password: "1234567", password_confirmation: "1234567" }
+      user_params = { first_name: "Elizabeth", last_name: "Swan", email: "elizabethswan@blackpearl.com", password: "1234567", password_confirmation: "1234567" }
       @user = User.new(user_params)
 
       expect(@user.save).to be false
@@ -26,7 +26,7 @@ RSpec.describe User, type: :model do
 
     describe '.authenticate_with_credentials' do
       it "should return a user if email and password are correct" do
-        user_params = { first_name: "Commander", last_name: "Data", email: "data@starfleet.com", password: "12345678", password_confirmation: "12345678" }
+        user_params = { first_name: "Davy", last_name: "Jones", email: "davy@blackpearl.com", password: "12345678", password_confirmation: "12345678" }
         @user = User.new(user_params)
         @user.save
   
@@ -34,15 +34,15 @@ RSpec.describe User, type: :model do
       end
  
       it "should return nil if email is incorrect" do
-        user_params = { first_name: "Geordi", last_name: "LaForge", email: "g.laforge@starfleet.com", password: "12345678", password_confirmation: "12345678" }
+        user_params = { first_name: "James", last_name: "Norrington", email: "jamebl. ackpearlcom", password: "12345678", password_confirmation: "12345678" }
         @user = User.new(user_params)
         @user.save
   
-        expect(User.authenticate_with_credentials("glaforge@starfleet.com", "12345678")).to be nil
+        expect(User.authenticate_with_credentials("glaforge@blackpearl.com", "12345678")).to be nil
       end
   
       it "should return nil if password is incorrect" do
-        user_params = { first_name: "Beverly", last_name: "Crusher", email: "b.crusher@starfleetmedical.com", password: "12345678", password_confirmation: "12345678" }
+        user_params = { first_name: "Joshamee", last_name: "Gibbs", email: "joshamee@blackpearl.com", password: "12345678", password_confirmation: "12345678" }
         @user = User.new(user_params)
         @user.save
   
